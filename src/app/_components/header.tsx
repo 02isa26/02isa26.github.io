3a39ea636296197736e1123c2530f19a4cf1d058
02isa26 <isa02.shn26@gmail.com>
2024-05-22 02:29:49 +0900

    Change header design

--- src/app/_components/header.tsx
+++ src/app/_components/header.tsx
@@ -1,14 +1,21 @@
 import Link from "next/link";
 
 const Header = () => {
-  return (
-    <h2 className="text-2xl md:text-4xl font-bold tracking-tight md:tracking-tighter leading-tight mb-20 mt-8">
-      <Link href="/" className="hover:underline">
-        Blog
-      </Link>
-      .
-    </h2>
-  );
+    return (
+        <header className="text-gray-400 bg-gray-900 body-font">
+            <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+                <a className="flex title-font font-medium items-center text-white mb-4 md:mb-0">
+                    <img src="/tmp/155885475.png" className="w-11 h-11 rounded-full" />
+                    <span className="ml-3 text-xl">isa02のwebサイト</span>
+                </a>
+                <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
+                    <a className="mr-5 hover:text-white">Profile</a>
+                    <a className="mr-5 hover:text-white">Articles</a>
+                    <a className="mr-5 hover:text-white">Documents</a>
+                </nav>
+            </div>
+        </header>
+    );
 };
 
 export default Header;
