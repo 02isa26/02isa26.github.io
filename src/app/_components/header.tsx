93ad9657e65477386f4e263607f3ea91e287e0e4
02isa26 <isa02.shn26@gmail.com>
2024-05-25 04:35:58 +0900

    Change header design

--- src/app/_components/header.tsx
+++ src/app/_components/header.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 const Header = () => {
     return (
-        <header className="text-gray-400 bg-gray-900 body-font">
+        <header className="text-gray-400 bg-gray-800 body-font">
             <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
                 <a className="flex title-font font-medium items-center text-white mb-4 md:mb-0">
                     <img src="/tmp/155885475.png" className="w-11 h-11 rounded-full" />
