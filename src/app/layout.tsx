155f44d77ab297884a3bf0441efc59137a99e5b2
02isa26 <isa02.shn26@gmail.com>
2024-05-29 01:59:49 +0900

    Change language attribute to ja from en

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
     children: React.ReactNode;
 }>) {
     return (
-        <html lang="en">
+        <html lang="ja">
             <head>
                 <link
                     rel="apple-touch-icon"
