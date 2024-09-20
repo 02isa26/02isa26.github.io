c6a6ea9d0c727516b9a0e88d518fbc80349da0a4
02isa26 <isa02.shn26@gmail.com>
2024-09-21 08:32:29 +0900

    feat(conversion): Add rehype plugins to markdown -> html conversion process
    Add rehype-shiki to highlight code block.
    Add rehype-slug and rehype-autolink-headings to give an anchor link to headings.

--- src/lib/markdownToHtml.ts
+++ src/lib/markdownToHtml.ts
@@ -1,3 +1,6 @@
+import rehypeShiki from "@shikijs/rehype";
+import rehypeAutolinkHeadings from "rehype-autolink-headings";
+import rehypeSlug from "rehype-slug";
 import rehypeStringify from "rehype-stringify";
 import remarkParse from "remark-parse";
 import remarkRehype from "remark-rehype";
@@ -5,9 +8,12 @@ import { unified } from "unified";
 
 export default async function markdownToHtml(markdown: string) {
   const result = await unified()
-    .use(remarkParse)
-    .use(remarkRehype)
-    .use(rehypeStringify)
+    .use(remarkParse) // markdown -> mdast
+    .use(remarkRehype) // mdast -> hast
+    .use(rehypeSlug)
+    .use(rehypeAutolinkHeadings, { behavior: "wrap" })
+    .use(rehypeShiki, { theme: "solarized-dark" })
+    .use(rehypeStringify) // hast -> html
     .process(markdown);
   return result.toString();
 }
