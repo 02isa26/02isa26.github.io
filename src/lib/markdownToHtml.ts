8a7da898a91ed28514a8ce5c8598f315daafe859
02isa26 <isa02.shn26@gmail.com>
2024-09-21 08:38:17 +0900

    feat(conversion): Add remark-gfm plugin and set footnote related options
    Add remark-gfm plugin to markdown -> html conversion process.
    Set footnote related options in remark-rehype plugin. clobberPrefix refers to the prefix for the reference number's id, but here it's an empty string, so the id is simply "fnref-<reference-number>".

--- src/lib/markdownToHtml.ts
+++ src/lib/markdownToHtml.ts
@@ -3,6 +3,7 @@ import rehypeShiki from "@shikijs/rehype";
 import rehypeAutolinkHeadings from "rehype-autolink-headings";
 import rehypeSlug from "rehype-slug";
 import rehypeStringify from "rehype-stringify";
+import remarkGfm from "remark-gfm";
 import remarkParse from "remark-parse";
 import remarkRehype from "remark-rehype";
 import { unified } from "unified";
@@ -10,7 +11,13 @@ import { unified } from "unified";
 export default async function markdownToHtml(markdown: string) {
   const result = await unified()
     .use(remarkParse) // markdown -> mdast
-    .use(remarkRehype) // mdast -> hast
+    .use(remarkGfm, { singleTilde: false })
+    .use(remarkRehype, {
+      clobberPrefix: "",
+      footnoteLabel: "脚注",
+      footnoteLabelProperties: { className: null },
+      footnoteLabelTagName: "h2",
+    }) // mdast -> hast
     .use(rehypeSectionize, {
       properties: { className: null },
       enableRootSection: false,
