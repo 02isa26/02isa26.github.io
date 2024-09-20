bbba46689f3ef570c57e0145c8ceff18be706e77
02isa26 <isa02.shn26@gmail.com>
2024-09-21 08:17:49 +0900

    feat(article): Add abstract prop to display article abstract

--- src/app/_components/ArticleHeader.tsx
+++ src/app/_components/ArticleHeader.tsx
@@ -3,17 +3,22 @@ import DateFormatter from "@/app/_components/DateFormatter";
 type Props = {
   title: string;
   date: string;
+  abstract: string;
 };
 
-export function ArticleHeader({ title, date }: Props) {
+export function ArticleHeader({ title, date, abstract }: Props) {
   return (
     <>
       <div className="mb-2 text-left font-title text-2xl">
         <DateFormatter dateString={date} />
       </div>
-      <h1 className="mb-14 text-left font-title text-3xl font-bold leading-tight tracking-tighter">
-        {title}
-      </h1>
+      <h1 className="mb-4 text-left font-title text-3xl font-bold leading-tight">{title}</h1>
+      {abstract && (
+        <div className="mb-4">
+          <h2 className="font-title text-2xl font-bold">概要</h2>
+          <p className="font-body text-lg tracking-tighter">{abstract}</p>
+        </div>
+      )}
     </>
   );
 }
