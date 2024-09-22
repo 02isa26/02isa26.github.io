c410964d932f624f0c383df97e428d85c88c42a4
02isa26 <isa02.shn26@gmail.com>
2024-09-23 00:38:38 +0900

    feat(ux): Replace anchor tags with next/link
    Enhance the navigation in header by using next/link component.
    Update details: font, link, and mergin for better experience.

--- src/app/_components/Header.tsx
+++ src/app/_components/Header.tsx
@@ -1,20 +1,25 @@
 import myIcon from "@@/public/assets/myIcon.png";
 import Image from "next/image";
+import Link from "next/link";
 
 const Header = () => {
   return (
     <header className="bg-gray-800">
-      <div className="container mx-auto flex flex-col flex-wrap items-center p-5 font-geistSans md:flex-row">
-        <a className="mb-4 flex items-center font-title font-medium text-white md:mb-0">
-          <Image src={myIcon} alt="my icon" priority={true} className="size-10 rounded-full" />
-          <span className="ml-3 font-title text-xl font-bold">isa02の個人サイト</span>
-        </a>
-        <nav className="flex flex-wrap items-center justify-center text-base text-gray-400 md:ml-auto">
-          <a className="mr-5 hover:text-white">Profile</a>
-          <a className="mr-5 hover:text-white" href="blog/articles/">
+      <div className="container mx-auto flex flex-col flex-wrap items-center p-5 font-sub md:flex-row">
+        <Link href="/" className="mb-3 flex items-center md:mb-0">
+          <Image src={myIcon} alt="" priority={true} className="size-9 rounded-full sm:size-10" />
+          <span className="ml-3 text-xl text-white">isa02の個人サイト</span>
+        </Link>
+        <nav className="mx-auto flex flex-wrap items-center justify-center text-base text-gray-400 md:mr-0">
+          <Link className="mr-5 hover:text-white" href="/profile">
+            Profile
+          </Link>
+          <Link className="mr-5 hover:text-white" href="/blog/articles">
             Articles
-          </a>
-          <a className="mr-5 hover:text-white">Documents</a>
+          </Link>
+          <Link className="hover:text-white" href="/documents">
+            Documents
+          </Link>
         </nav>
       </div>
     </header>
